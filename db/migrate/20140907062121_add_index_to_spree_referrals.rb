57a7b9ac5c8b289d21a505c6443a685cac1a51f3
Datasoftok <85146818+Datasoftok@users.noreply.github.com>
2021-06-23 17:26:19 -0300

    Update 20140907062121_add_index_to_spree_referrals.rb

--- db/migrate/20140907062121_add_index_to_spree_referrals.rb
+++ db/migrate/20140907062121_add_index_to_spree_referrals.rb
@@ -1,4 +1,4 @@
-class AddIndexToSpreeReferrals < ActiveRecord::Migration[4.2]
+class AddIndexToSpreeReferrals < ActiveRecord::Migration[6.0]
   def change
     add_index :spree_referrals, :user_id
   end
